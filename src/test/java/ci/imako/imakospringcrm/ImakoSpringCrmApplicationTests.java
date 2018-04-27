72685bb4037ca276a19944c13d8c5c07d571f4b2
Traore Mohamed <mohamed.s.c.traore@gmail.com>
2018-04-27 16:57:04 +0000

    Premier commit du projet

---
+++ src/test/java/ci/imako/imakospringcrm/ImakoSpringCrmApplicationTests.java
@@ -0,0 +1,16 @@
+package ci.imako.imakospringcrm;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ImakoSpringCrmApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
